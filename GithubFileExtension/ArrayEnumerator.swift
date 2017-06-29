b5f38a849c294c12c8f4dcf23607bbdb1a80ad1d
mzp <mzpppp@gmail.com>
2017-06-29 20:30:56 +0900

    :lipstick: add simple NSFileProviderEnumerator

--- GithubFileExtension/ArrayEnumerator.swift
+++ GithubFileExtension/ArrayEnumerator.swift
@@ -9,43 +9,18 @@
 import FileProvider
 
 class ArrayEnumerator: NSObject, NSFileProviderEnumerator {
-    
-    var enumeratedItemIdentifier: NSFileProviderItemIdentifier
-    
-    init(enumeratedItemIdentifier: NSFileProviderItemIdentifier) {
-        self.enumeratedItemIdentifier = enumeratedItemIdentifier
+    private let items : [NSFileProviderItemProtocol]
+
+    init(items: [NSFileProviderItemProtocol]) {
+        self.items = items
         super.init()
     }
 
-    func invalidate() {
-        // TODO: perform invalidation of server connection if necessary
+    func enumerateItems(for observer: NSFileProviderEnumerationObserver, startingAtPage page: Data) {
+        observer.didEnumerate(items)
+        observer.finishEnumerating(upToPage: nil)
     }
 
-    func enumerateItems(for observer: NSFileProviderEnumerationObserver, startingAtPage page: Data) {
-        /* TODO:
-         - inspect the page to determine whether this is an initial or a follow-up request
-         
-         If this is an enumerator for a directory, the root container or all directories:
-         - perform a server request to fetch directory contents
-         If this is an enumerator for the active set:
-         - perform a server request to update your local database
-         - fetch the active set from your local database
-         
-         - inform the observer about the items returned by the server (possibly multiple times)
-         - inform the observer that you are finished with this page
-         */
+    func invalidate() {
     }
-    
-    func enumerateChanges(for observer: NSFileProviderChangeObserver, fromSyncAnchor anchor: Data) {
-        /* TODO:
-         - query the server for updates since the passed-in sync anchor
-         
-         If this is an enumerator for the active set:
-         - note the changes in your local database
-         
-         - inform the observer about item deletions and updates (modifications + insertions)
-         - inform the observer when you have finished enumerating up to a subsequent sync anchor
-         */
-    }
-    
 }
