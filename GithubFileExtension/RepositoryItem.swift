a284af968c7d0b54a6045e3c3ba590bd643b00c6
mzp <mzpppp@gmail.com>
2017-06-29 20:20:09 +0900

    :truck: FileProviderItem -> RepositoryItem

--- GithubFileExtension/RepositoryItem.swift
+++ GithubFileExtension/RepositoryItem.swift
@@ -8,7 +8,7 @@
 
 import FileProvider
 
-class FileProviderItem: NSObject, NSFileProviderItem {
+class RepositoryItem: NSObject, NSFileProviderItem {
 
     // TODO: implement an initializer to create an item from your extension's backing model
     // TODO: implement the accessors to return the values from your extension's backing model
