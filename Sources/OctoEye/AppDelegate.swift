c169821032d3552548446cf3f58a4fe33f16fa18
mzp <mzpppp@gmail.com>
2017-07-09 20:42:48 +0900

    :fire: remove storyboard

--- Sources/OctoEye/AppDelegate.swift
+++ Sources/OctoEye/AppDelegate.swift
@@ -11,11 +11,12 @@ import UIKit
 // swiftlint:disable line_length
 @UIApplicationMain
 internal class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
