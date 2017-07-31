2596557b6a51eb5449b51ea2e10200848e06ed3d
mzp <mzpppp@gmail.com>
2017-08-01 06:55:55 +0900

    :construction: add repositories view(not yet implemented)

---
+++ Sources/OctoEye/Controller/RepositoriesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RepositoriesViewController.swift
+//  OctoEye
+//
+//  Created by mzp on 2017/08/01.
+//  Copyright © 2017 mzp. All rights reserved.
+//
+
+import UIKit
+
+internal class RepositoriesViewController: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        self.title = "Repositories"
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) is not implemented")
+    }
+}
