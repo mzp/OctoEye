a5cb1660aa73b9efee142e59ec01e7eb44df4603
mzp <mzpppp@gmail.com>
2017-08-07 08:02:42 +0900

    :art: show ripple mark at hint point

--- Sources/OctoEye/Controller/WalkThroughViewController.swift
+++ Sources/OctoEye/Controller/WalkThroughViewController.swift
@@ -11,6 +11,12 @@ import Ikemen
 import UIKit
 
 internal class WalkThroughViewController: UIViewController, EAIntroDelegate {
+    private var rippleView: SMRippleView?
+    private let ripplePoints: [UInt:CGPoint] = [
+        1: CGPoint(x: 257, y: 187),
+        2: CGPoint(x: 224, y: 267)
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,7 +43,7 @@ internal class WalkThroughViewController: UIViewController, EAIntroDelegate {
 
         view.backgroundColor = UIColor.white
         let introView = EAIntroView(frame: view.frame, andPages: [page1, page2, page3])
-        introView?.contentMode = .center
+        introView?.bgViewContentMode = .center
         introView?.skipButton.setTitleColor(UIColor.black, for: .normal)
         introView?.pageControl.currentPageIndicatorTintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         introView?.pageControl.pageIndicatorTintColor = #colorLiteral(red: 0.6642268896, green: 0.6642268896, blue: 0.6642268896, alpha: 1)
@@ -62,5 +68,38 @@ internal class WalkThroughViewController: UIViewController, EAIntroDelegate {
         let viewController = LoginViewController()
         viewController.modalTransitionStyle = .crossDissolve
         present(viewController, animated: true) {}
+    }
+
+    // swiftlint:disable:next implicitly_unwrapped_optional
+    func intro(_ introView: EAIntroView!, pageAppeared page: EAIntroPage!, with pageIndex: UInt) {
+        NSLog("pageAppered")
+        self.rippleView?.stopAnimation()
+        self.rippleView?.removeFromSuperview()
+
+        guard let point = hintPoint(introView, pageIndex: pageIndex) else {
+            return
+        }
+        let v = SMRippleView(frame: CGRect(origin: point,
+                                           size: CGSize(width: 44, height: 44)),
+                             color: #colorLiteral(red: 0.3450980392, green: 0.3450980392, blue: 0.3450980392, alpha: 0.2),
+                             interval: 2,
+                             duration: 4)
+        view.addSubview(v)
+
+        rippleView = v
+    }
+
+    private func hintPoint(_ introView: EAIntroView, pageIndex: UInt) -> CGPoint? {
+        guard let point = ripplePoints[pageIndex] else {
+            return nil
+        }
+        guard let page = introView.pages[Int(pageIndex)] as? EAIntroPage else {
+            return nil
+        }
+        let parentSize = view.bounds.size
+        let imageSize = page.bgImage.size
+        return point.applying(
+            CGAffineTransform(translationX: parentSize.width / 2 - imageSize.width / 2,
+                              y: parentSize.height / 2 - imageSize.height / 2))
     }
 }
